6f13430fa21c9026f424b63cebbd682ae5acd12f
Brunno <brunocavalcantedsilva@gmail.com>
2019-10-24 20:12:26 -0300

    teste

--- app/src/main/java/com/example/salaoapp/LoginActivity.java
+++ app/src/main/java/com/example/salaoapp/LoginActivity.java
@@ -5,13 +5,17 @@ import android.view.View;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 
 import Model.Usuario;
+import Services.UserService;
 
 public class LoginActivity extends AppCompatActivity {
 
     Button btn_login;
     Button btn_cadastro;
+    EditText login;
+    EditText senha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,14 +24,19 @@ public class LoginActivity extends AppCompatActivity {
 
         btn_login = (Button) findViewById(R.id.btnLogin);
         btn_cadastro = (Button) findViewById(R.id.btnCadastro);
+        login = findViewById(R.id.editTextLogin);
+        senha = findViewById(R.id.editTextSenhaLogin);
 
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-
                 Usuario usuario = new Usuario();
-                usuario.setLogin();
+                usuario.setLogin(login.getText().toString());
+                usuario.setSenha(senha.getText().toString());
+
+                UserService usuarioWS = new UserService();
+                usuarioWS.singInUser(usuario);
                 Intent it = new Intent(LoginActivity.this,MainActivity.class);
                 startActivity(it);
             }
